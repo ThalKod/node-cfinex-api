7cfec5ecb39002c1be662f9ac616085e518928a5
ThalKod <tmarcelin@thal.tech>
2018-08-06 13:15:52 -0400

    add defaultRequestOptions

--- node-binance-api.js
+++ node-binance-api.js
@@ -5,3 +5,25 @@
  * Copyright 2018-, ThalKod
  * Released under the MIT License
  * ============================================================ */
+
+ let api = function cfinex(){
+    const request = require("request");
+
+    const defaultRequestOptions = {
+        method: "GET",
+        agent: false,
+        headers: {
+          "User-Agent": "Mozilla/4.0 (compatible; Node CFINEX API)",
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      };
+
+      const opts = {
+        baseUrl: "http://cfinex.com/",
+        apikey: "APIKEY",
+        apisecret: "APISECRET",
+      };
+ };
+
+
+ module.exports = api;
